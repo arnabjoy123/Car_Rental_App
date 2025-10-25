1af0dc459d0dcd6bc4ad8d7002e834183ae73340
Arnab Das <arnab.das.34.1@mountblue.tech>
2025-10-25 20:56:17 +0530

    setup redux store and authSlice for sign up, sign in and logout

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,99 @@
-import { View, Text, Button } from 'react-native';
 import React from 'react';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import data from '../mocks/cars.json';
 
 const Home = () => {
   const navigation = useNavigation();
 
+  const renderCarItem = ({ item }) => (
+    <View style={styles.card}>
+      <Image source={{ uri: item.images[0] }} style={styles.image} />
+      <Text style={styles.title}>
+        {item.make} {item.model} ({item.year})
+      </Text>
+      <Text style={styles.details}>
+        Seats: {item.seats} | Transmission: {item.transmission}
+      </Text>
+      <Text style={styles.price}>${item.pricePerDay} / day</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>View Details</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>Home</Text>
-      <Button
-        title="Go to Arnabs profile"
-        onPress={() => navigation.navigate('Profile', { name: 'Arnab' })}
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={item => item.id.toString()}
+        renderItem={renderCarItem}
+        contentContainerStyle={{ paddingBottom: 16 }}
       />
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 16,
+    backgroundColor: 'black',
+  },
+  card: {
+    backgroundColor: '#ffffffff',
+    borderRadius: 12,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: '#fff',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 3,
+  },
+  image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 12,
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  details: {
+    fontSize: 14,
+    color: '#555',
+    marginBottom: 8,
+  },
+  price: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFA001',
+    marginBottom: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  button: {
+    backgroundColor: 'black',
+    height: 40,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
