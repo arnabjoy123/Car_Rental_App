e4794095ef2643f94549787b0c2d21029b097147
Arnab Das <arnab.das.34.1@mountblue.tech>
2025-10-25 12:00:48 +0530

    fix: Madde separate navigation folder for clarity separate from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,43 +20,14 @@ import {
 } from 'react-native-safe-area-context';
 import Home from './screens/Home';
 import Profile from './screens/Profile';
-import { createStaticNavigation } from '@react-navigation/native';
+import {
+  createStaticNavigation,
+  NavigationContainer,
+} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Bookings from './screens/Bookings';
 import Onboarding from './screens/Onboarding';
-// import Feather from 'react-native-vector-icons';
-
-const MyTabs = createBottomTabNavigator({
-  screens: {
-    Home: {
-      screen: Home,
-      options: {
-        tabBarLabel: 'Home',
-        // tabBarIcon: ({ color, size }) => (
-        //   // <Feather name="home" color={color} size={size} />
-        // ),
-      },
-    },
-    Bookings: Bookings,
-    Profile: Profile,
-  },
-});
-
-const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Onboarding',
-  screens: {
-    Onboarding: {
-      screen: Onboarding,
-      options: { title: 'Welcome' },
-    },
-    MainTabs: {
-      screen: MyTabs,
-      options: { headerShown: false },
-    },
-  },
-});
-
-const Navigation = createStaticNavigation(RootStack);
+import { RootNavigator } from './navigation';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -64,35 +35,11 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      {/* <AppContent /> */}
-      <Navigation />
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hiiiii</Text>
-      {/* <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      /> */}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-  text: {
-    color: 'white',
-    fontSize: 60,
-  },
-});
 
 export default App;
---
+++ src/navigation/index.ts
@@ -0,0 +1,3 @@
+// src/navigation/index.js
+export { default as RootNavigator } from './RootNavigator';
+export { default as TabNavigator } from './TabNavigator';
