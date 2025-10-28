ab2fbb02bc32fd8d9d05ac227661fc2d659069b4
Arnab Das <arnab.das.34.1@mountblue.tech>
2025-10-28 17:45:46 +0530

    added single details page and export functionality

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,10 +1,18 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigator from './TabNavigator';
+import SingleDetailsPage from '../screens/SingleDetailsPage';
+import BookingForm from '../screens/BookingForm';
 
 const Stack = createNativeStackNavigator();
 
 export const AppStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="MainTabs" component={TabNavigator} />
+    <Stack.Screen
+      name="SingleDetailsPage"
+      options={{ presentation: 'modal' }}
+      component={SingleDetailsPage}
+    />
+    <Stack.Screen name="BookingForm" component={BookingForm} />
   </Stack.Navigator>
 );
